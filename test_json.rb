6b33d056731d72f2f63525ab7bc54a1d3a915060
rmarini <rmarini@uiowa.edu>
2018-07-14 14:03:12 -0500

    correctly getting all creatures now

--- test_json.rb
+++ test_json.rb
@@ -13,13 +13,28 @@ LOG.level = Logger::INFO
 ##stand_in_ARGV1 = ARGV[1]
 
 #for static input
-stand_in_ARGV0 = "dev_misc/improved-initiative.json" #what to manipulate
+stand_in_ARGV0 = "dev_misc/Rob_5e_improved-initiative.json" #what to manipulate
 #stand_in_ARGV1 = "dev_misc/Astorav2.2.html" #new file name and location
 
 ii_file = File.read(stand_in_ARGV0)
 ii_json = JSON.parse(ii_file)
-test_npc_encounter_folder = JSON.parse(ii_json['ImprovedInitiative.SavedEncounters.npc_encounter_folder-npc_encounter'])
 
+dragons = Array.new
+creatures = Hash.new
+ii_json.keys.each {|k|
+  if(k.include? "Creatures.")
+    temp = JSON.parse(ii_json[k]);
+    creatures[temp["Name"]] = temp;
+    
+    if(temp["Name"].downcase.include? "dragon")
+      dragons.push(temp["Name"]);
+    end
+      
+  end
+  
+}
 
+puts(dragons);
+puts(creatures["Green Gaping Gutter Dragon"]);
 LOG.info("json reading complete")
 LOG.info("see: #{output_loc}")
